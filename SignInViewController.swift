1bd9de5bbab9e200950a133e373bd6a09948cf6e
Gabriele Castaldo <gabrielecastaldo@Gabrieles-MacBook-Air.local>
2016-11-17 19:42:05 +0800

    add after successful sign in go back to login page

--- SignInViewController.swift
+++ SignInViewController.swift
@@ -14,6 +14,9 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var emailSignInText: UITextField!
     @IBOutlet weak var passwordSignInText: UITextField!
     
+    var validEmail = false
+    var validPassword = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -56,37 +59,48 @@ class SignInViewController: UIViewController {
         }
     }
     
-        @IBAction func singInAction(_ sender: Any) {
+    @IBAction func singInAction(_ sender: Any) {
         
         if isValidEmail(testStr: emailSignInText.text!) == true{
             
-            print("email questa va ben")
+            validEmail = true
+            print("email good")
             
         }else{
             
-            print("email na merda di mail")
+            let alert = UIAlertController(title: "Invalid Email", message: "Enter a valid email", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
             
         }
         
         if isValidPassword(testStr: passwordSignInText.text!) == true{
-                
-            print(isValidPassword(testStr: passwordSignInText.text!))
+            
+            validPassword = true
+            print("password good")
+            
         }else{
             
-            let alert = UIAlertController(title: "Invalid Password", message: "Password must contain at least a number and a capital letter", preferredStyle: UIAlertControllerStyle.alert)
+            let alert = UIAlertController(title: "Invalid Password", message: "Password must be between 8 and 16 characters and contain at least a number and a capital letter", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
 
-            
-//            print(isValidPassword(testStr: passwordSignInText.text!))
         }
         
-//        FIRAuth.auth()?.createUser(withEmail: emailSignInText.text!, password: passwordSignInText.text!) { (user, error) in
-//           
-//            
-//            
-//        }
-//        
+        if validEmail == true && validPassword == true {
+            
+//            FIRAuth.auth()?.createUser(withEmail: emailSignInText.text!, password: passwordSignInText.text!) { (user, error) in
+            
+            let alert = UIAlertController(title: "Very good", message: "You have successfully sign in", preferredStyle: UIAlertControllerStyle.alert)
+            let goBackAction = UIAlertAction(title: "Go to Login", style: .default) { (action) in
+                self.performSegue(withIdentifier: "GoToLogin", sender: nil)
+                
+            }
+            alert.addAction(goBackAction)
+            self.present(alert, animated: true, completion: nil)
+            //self.dismiss(animated: true, completion: nil)
+            
+        }
         
         
     }
