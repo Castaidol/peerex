c7e4d3a4111f5df0b8f3b63d8b327cd607bba0e6
Gabriele Castaldo <gabrielecastaldo@Gabrieles-MacBook-Air.local>
2016-11-22 21:09:14 +0800

    auth: check existing email, confirm password

--- SignInViewController.swift
+++ SignInViewController.swift
@@ -13,9 +13,11 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailSignInText: UITextField!
     @IBOutlet weak var passwordSignInText: UITextField!
+    @IBOutlet weak var reTypePassword: UITextField!
     
     var validEmail = false
     var validPassword = false
+    var samePassword = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,27 +38,41 @@ class SignInViewController: UIViewController {
         return emailTest.evaluate(with: testStr)
     }
     
-    func isValidPassword(testStr:String) -> Bool{
+    func isValidPassword(testPass:String) -> Bool{
         
-        if testStr.characters.count >= 8 && testStr.characters.count <= 16{
+        if testPass.characters.count >= 8 && testPass.characters.count <= 16{
         
             let capNumLowRegEx = "[A-Z0-9a-z]+"
             var passwordTest = NSPredicate(format: "SELF MATCHES %@", capNumLowRegEx)
-            let capNumLowresult = passwordTest.evaluate(with: testStr)
+            let capNumLowresult = passwordTest.evaluate(with: testPass)
         
             let NumRegEx = ".*[0-9]+.*"
             passwordTest = NSPredicate(format: "SELF MATCHES %@", NumRegEx)
-            let numResult = passwordTest.evaluate(with: testStr)
+            let numResult = passwordTest.evaluate(with: testPass)
         
             let capRegEx = ".*[A-Z]+.*"
             passwordTest = NSPredicate(format: "SELF MATCHES %@", capRegEx)
-            let capresult = passwordTest.evaluate(with: testStr)
+            let capresult = passwordTest.evaluate(with: testPass)
         
             return capNumLowresult && numResult && capresult
         }else{
             
             return false
         }
+    }
+    
+    func confirmPassword(pass: String, reType: String ) -> Bool{
+        
+        if pass == reType{
+            
+            return true
+            
+            
+        }else{
+            
+            return false
+        }
+        
     }
     
     @IBAction func singInAction(_ sender: Any) {
@@ -72,9 +88,11 @@ class SignInViewController: UIViewController {
             alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             
+            return
+            
         }
         
-        if isValidPassword(testStr: passwordSignInText.text!) == true{
+        if isValidPassword(testPass: passwordSignInText.text!) == true{
             
             validPassword = true
             print("password good")
@@ -84,27 +102,53 @@ class SignInViewController: UIViewController {
             let alert = UIAlertController(title: "Invalid Password", message: "Password must be between 8 and 16 characters and contain at least a number and a capital letter", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
+            
+            return
 
         }
         
-        if validEmail == true && validPassword == true {
+        if confirmPassword(pass: passwordSignInText.text!, reType: reTypePassword.text!){
             
-//            FIRAuth.auth()?.createUser(withEmail: emailSignInText.text!, password: passwordSignInText.text!) { (user, error) in
+            samePassword = true
+            print("same password")
+        }else{
             
-            let alert = UIAlertController(title: "Very good", message: "You have successfully sign in", preferredStyle: UIAlertControllerStyle.alert)
-            let goBackAction = UIAlertAction(title: "Go to Login", style: .default) { (action) in
-                self.performSegue(withIdentifier: "GoToLogin", sender: nil)
-                
-            }
-            alert.addAction(goBackAction)
+            let alert = UIAlertController(title: "Invalid Password", message: "Password and Confirm Password are not the same", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
-            //self.dismiss(animated: true, completion: nil)
+            
+            return
             
         }
+    
         
-        
-    }
+        if validEmail == true && validPassword == true && samePassword == true  {
+            
+            FIRAuth.auth()?.createUser(withEmail: emailSignInText.text!, password: passwordSignInText.text!) { (user, error) in
+                
+                if error == nil{
+                    
+                    let alert = UIAlertController(title: "Very good", message: "You have successfully sign in", preferredStyle: UIAlertControllerStyle.alert)
+                    let goBackAction = UIAlertAction(title: "Go to Login", style: .default) { (action) in
+                        self.performSegue(withIdentifier: "GoToLogin", sender: nil)
+                        
+                    }
+                    alert.addAction(goBackAction)
+                    self.present(alert, animated: true, completion: nil)
+                    
+                }else if let errCode = FIRAuthErrorCode(rawValue: (error?._code)!){
+                    
+                    if errCode == .errorCodeEmailAlreadyInUse {
+                        
+                        let alert = UIAlertController(title: "Invalid Email", message: "The email address is already in use by another account", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
 
-    
-    
+                    }
+                }
+            }
+        }
+    }
 }
+
+
